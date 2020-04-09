735880ea86f515788ed587346309ae5f87b1fb7f
stebo <steven.bowman@tr.com>
2020-04-09 09:09:15 -0400

    fixed missing comma

--- msPatch.py
+++ msPatch.py
@@ -71,7 +71,7 @@ while "200" not in str(response):
     if elapsedtime > 3600:
         #send report on linux
         if platform == "linux":
-            os.system("echo '' | mutt -s 'MSPatch :: SCRIPT RUNNING; NO RESULTS"+cfg['email'])
+            os.system("echo '' | mutt -s 'MSPatch :: SCRIPT RUNNING; NO RESULTS'"+cfg['email'])
         print("Email sent, still no results")
 
     print(str(datetime.now())+' :: No Report Found. Waiting 5 min.', end='\r')
