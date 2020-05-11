aeb8cf1dc2bc522353c765f94bb1913f38070e83
stebo <steven.bowman@tr.com>
2020-05-11 08:38:05 -0400

    added Tuesday Logic.

--- autoRunMSpatch.sh
+++ autoRunMSpatch.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
-export https_proxy=http://webproxy.f.corp.services:80
-export http_proxy=http://webproxy.f.corp.services:80
-source /data/scripts/asr/bin/activate
-myvar="$PWD"
-cd /data/scripts/msPatch/
-python3 ./msPatch.py
-deactivate
-cd "$myvar"
+if [ $(date '+%a') = "Tue" ]; then
+  export https_proxy=http://webproxy.f.corp.services:80
+  export http_proxy=http://webproxy.f.corp.services:80
+  source /data/scripts/asr/bin/activate
+  myvar="$PWD"
+  cd /data/scripts/msPatch/
+  python3 ./msPatch.py
+  deactivate
+  cd "$myvar"
+else
+   echo "Not Tuesday"
+   exit 1;
